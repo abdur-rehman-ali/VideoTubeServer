14873b300be082683a06fe6df39a5a93493c2cc8
abdur-rehman-ali <alijoyia911@gmail.com>
2024-04-07 19:14:47 -0700

    feat: Add multer middleware for files and form data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,15 @@ import { User } from "../models/user.model.js"
 export const registerUser = async (req, res) => {
   try {
     const { userName, email, password, firstName, lastName } = req.body
-    const user = await User.create({
+    const { profileImage, coverImage } = req.files
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(201).json({message: "User with this email already exists"})
+    }
+
+    await User.create({
       userName,
       email,
       password,
@@ -11,7 +19,7 @@ export const registerUser = async (req, res) => {
       lastName,
       profileImage: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
     })
-    res.status(200).json({ userName, email, password, firstName, lastName })
+    res.status(200).json({ userName, email, password, firstName, lastName, profileImage: profileImage[0].path, converImage: coverImage[0].path })
   } catch (error) {
     res.status(401).json({ error: error.message })
   }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import { upload } from '../utils/multer.js';
 import { registerUser } from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.route('/register').post(registerUser)
+router.route('/register').post(upload.fields([
+  {name: 'profileImage', maxCount: 1},
+  {name: 'coverImage', maxCount: 1},
+]), registerUser)
 
 export default router;
