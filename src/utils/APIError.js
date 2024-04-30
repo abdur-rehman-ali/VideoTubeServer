1f5b05580e60c612d10287be1544885f4050f388
abdur-rehman-ali <alijoyia911@gmail.com>
2024-04-29 17:25:24 -0700

    chore: async handler and api error

---
+++ src/utils/APIError.js
@@ -0,0 +1,19 @@
+export class APIError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.success = false;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
