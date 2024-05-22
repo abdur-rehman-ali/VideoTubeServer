026060217b347b6fdd76c88cc9d5879e96564db2
abdur-rehman-ali <alijoyia911@gmail.com>
2024-05-22 15:26:19 -0700

    Password reset endpoint

--- src/utils/APIResponse.js
+++ src/utils/APIResponse.js
@@ -3,6 +3,6 @@ export class APIResponse {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
-    this.sucess = statusCode < 400;
+    this.success = statusCode < 400;
   }
 }
