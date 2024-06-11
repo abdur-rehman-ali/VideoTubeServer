62ac1fd18d37e5f9c16f4531694b08e60bc5c123
abdur-rehman-ali <alijoyia911@gmail.com>
2024-06-10 17:46:32 -0700

    Endpoint to edit, delete and fetch all tweets

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -3,6 +3,20 @@ import { APIError } from "../utils/APIError.js";
 import { APIResponse } from "../utils/APIResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+export const tweets = asyncHandler(async (req, res) => {
+  const tweets = await Tweet.find();
+
+  res
+    .status(200)
+    .json(
+      new APIResponse(
+        200,
+        { tweets, count: tweets.length },
+        "Tweets fetched successfully!"
+      )
+    );
+});
+
 export const createTweet = asyncHandler(async (req, res) => {
   const { content } = req.body;
 
@@ -25,4 +39,54 @@ export const createTweet = asyncHandler(async (req, res) => {
     .json(
       new APIResponse(201, tweetWithCreator, "Tweet created successfully!")
     );
+});
+
+export const editTweet = asyncHandler(async (req, res) => {
+  const { tweetID } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new APIError(400, "Content is required");
+  }
+
+  const tweet = await Tweet.findByIdAndUpdate(
+    tweetID,
+    {
+      content: content
+    },
+    { new: true }
+  );
+
+  const tweetWithCreator = await tweet.populate(
+    "creator",
+    "-password -refreshToken"
+  );
+
+  res
+    .status(201)
+    .json(
+      new APIResponse(201, tweetWithCreator, "Tweet updated successfully!")
+    );
+});
+
+export const deleteTweet = asyncHandler(async (req, res) => {
+  const { tweetID } = req.params;
+
+  if (!tweetID) {
+    throw new APIError(400, "Tweet ID is required");
+  }
+
+  const tweet = await Tweet.findById(tweetID);
+  if (!tweet) {
+    throw new APIError(404, "Tweet not found");
+  }
+
+  const isUserCreatorOfTweet = tweet.creator.equals(req.user.id);
+  if (!isUserCreatorOfTweet) {
+    throw new APIError(403, "You are not authorized to delete this tweet");
+  }
+
+  await tweet.deleteOne();
+
+  res.status(200).json(new APIResponse(200, {}, "Tweet deleted successfully!"));
 });
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,11 +1,29 @@
 import { Router } from "express";
-import { authenticate } from "../middlewares/authenticate.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import {
+  authenticate,
+  isUserAuthenticated
+} from "../middlewares/authenticate.middleware.js";
+import {
+  createTweet,
+  deleteTweet,
+  editTweet,
+  tweets
+} from "../controllers/tweet.controller.js";
 import { upload } from "../utils/multer.js";
 
 const router = Router();
+//Public routes
+router.route("/all").get(tweets);
 
 // Private routes
-router.route("/create").post(upload.none(), authenticate, createTweet);
+router
+  .route("/create")
+  .post(upload.none(), authenticate, isUserAuthenticated, createTweet);
+router
+  .route("/:tweetID/edit")
+  .put(upload.none(), authenticate, isUserAuthenticated, editTweet);
+router
+  .route("/:tweetID/delete")
+  .delete(authenticate, isUserAuthenticated, deleteTweet);
 
 export default router;
